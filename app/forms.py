11ab52edd0230141ba21e4de77b2876c7917dcbc
Simon Bösch <simon-andreas.boesch@hotmail.com>
2019-08-10 17:17:29 +0200

    Add user auth & edit

---
+++ app/forms.py
@@ -0,0 +1,87 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, ValidationError
+from wtforms.fields.html5 import EmailField
+from wtforms.validators import DataRequired, Length, Email
+from .models import User, db_session
+import hashlib
+import flask
+
+
+class LoginForm(FlaskForm):
+
+    username = StringField('Benutzername', [DataRequired()])
+    password = PasswordField('Passwort', [DataRequired()])
+
+    def validate(self):
+        if not super(LoginForm, self).validate():
+            return False
+        user = db_session.query(User).filter_by(
+            username=self.username.data).first()
+        if user is None:
+            self.password.errors.append(
+                'Benutzername oder Passwort ist falsch.')
+            return False
+        password_hash = hashlib.pbkdf2_hmac(
+            'sha256', self.password.data.encode('UTF-8'), user.password_salt.encode('UTF-8'), 1000)
+        if password_hash != user.password_hash:
+            self.password.errors.append(
+                'Benutzername oder Passwort ist falsch.')
+            return False
+
+        return True
+
+
+class EditUserForm(FlaskForm):
+    username = StringField('Benutzername', [DataRequired(), Length(max=100)])
+    email = EmailField('Email', [DataRequired(), Email()])
+    first_name = StringField('Vorname', [DataRequired(), Length(max=100)])
+    family_name = StringField('Nachname', [DataRequired(), Length(max=100)])
+
+    def validate_username(self, field):
+        user = db_session.query(User).filter_by(
+            id=flask.session['user_id']).first()
+        if field.data != user.username and db_session.query(User).filter_by(username=field.data).first() is not None:
+            raise ValidationError('Benutzername existiert bereits.')
+
+
+class RegisterForm(FlaskForm):
+
+    username = StringField('Benutzername', [DataRequired(), Length(max=100)])
+    email = EmailField('Email', [DataRequired(), Email()])
+    first_name = StringField('Vorname', [DataRequired(), Length(max=100)])
+    family_name = StringField('Nachname', [DataRequired(), Length(max=100)])
+    password = PasswordField('Passwort', [DataRequired(), Length(min=8)])
+
+    def validate_username(self, field):
+        username = field.data
+        user = db_session.query(User).filter_by(username=username).first()
+        if user is not None:
+            raise ValidationError('Benutzername bereits benutzt.')
+
+class CheckboxListWidget(object):
+
+    def __init__(self, stack=True, prefix_label=True):
+        assert html_tag in ("ol", "ul")
+        self.html_tag = html_tag
+        self.prefix_label = prefix_label
+
+    def __call__(self, field, **kwargs):
+        kwargs.setdefault("id", field.id)
+        html = ["<%s %s>" % (self.html_tag, html_params(**kwargs))]
+        for subfield in field:
+            if self.prefix_label:
+                html.append("<label>%s %s</label>" % (subfield.label, subfield()))
+            else:
+                html.append("<label>%s %s</label>" % (subfield(), subfield.label))
+        html.append("</%s>" % self.html_tag)
+        return Markup("".join(html))
+
+class MultiCheckboxField(SelectMultipleField):
+    widget = CheckboxListWidget()
+    option_widget = CheckboxInput()
+
+# class FormProject(FlaskForm):
+#         Code = StringField(
+#             'Code', [Required(message='Please enter your code')])
+#         Tasks = MultiCheckboxField('Proses', [Required(message='Please tick your task')], choices=[
+#                                     ('nyapu', 'Nyapu'), ('ngepel', 'Ngepel')])
---
+++ app/models.py
@@ -0,0 +1,110 @@
+from sqlalchemy import create_engine
+from sqlalchemy import Column, String, Integer, DateTime, ForeignKey, Table, Boolean
+from sqlalchemy.orm import relationship, sessionmaker, scoped_session
+from sqlalchemy.ext.declarative import declarative_base
+
+engine = create_engine('sqlite:///database.sqlite', echo=True)
+Base = declarative_base()
+
+GroupMembers = Table(
+    'GroupMembers', Base.metadata,
+    Column('user_id', ForeignKey('User.id'), primary_key=True),
+    Column('group_id', ForeignKey('Group.id'), primary_key=True),
+)
+
+
+GroupEventRelations = Table(
+    'GroupEventRelations', Base.metadata,
+    Column('group_id', ForeignKey('Group.id'), primary_key=True),
+    Column('event_id', ForeignKey('Event.id'), primary_key=True),
+)
+
+
+class Invitation(Base):
+    __tablename__ = 'Invitation'
+    event_id = Column(Integer, ForeignKey('Event.id'), primary_key=True)
+    user_id = Column(Integer, ForeignKey('User.id'), primary_key=True)
+    token = Column(String)
+    accepted = Column(Boolean)
+
+    event = relationship('Event', back_populates='invitations')
+    user = relationship('User', back_populates='invitations')
+
+
+class User(Base):
+    __tablename__ = 'User'
+    id = Column(Integer, primary_key=True)
+    username = Column(String, nullable=False)
+    email = Column(String, nullable=False)
+    first_name = Column(String, nullable=False)
+    family_name = Column(String, nullable=False)
+
+    password_salt = Column(String, nullable=False)
+    password_hash = Column(String, nullable=False)
+
+    # can create events and assign an admin
+    create_events_permissions = Column(Boolean)
+
+    # can create groups and assign an admin
+    create_groups_permissions = Column(Boolean)
+
+    groups = relationship('Group', secondary=GroupMembers,
+                          back_populates='users')
+    invitations = relationship('Invitation', back_populates='user')
+    administrated_events = relationship('Event', back_populates='admin')
+    administrated_groups = relationship('Group', back_populates='admin')
+
+    def __repr__(self):
+        return f'User(first_name={self.first_name}, family_name={self.family_name})'
+
+
+class Event(Base):
+    __tablename__ = 'Event'
+    id = Column(Integer, primary_key=True)
+    title = Column(String)
+    message = Column(String)
+    location = Column(String)
+    time = Column(DateTime)
+    equipment = Column(String)
+    money = Column(Integer)
+
+    send_invitations = Column(Boolean)
+    deadline = Column(DateTime)
+    created_at = Column(DateTime)
+    admin_id = Column(Integer, ForeignKey(User.id))
+
+    admin = relationship(User, back_populates='administrated_events')
+    groups = relationship(
+        'Group', secondary=GroupEventRelations, back_populates='events')
+    invitations = relationship('Invitation', back_populates='event')
+    updates = relationship(
+        'EventUpdate', order_by='EventUpdate.created_at', back_populates='event')
+
+
+class Group(Base):
+    __tablename__ = 'Group'
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    admin_id = Column(Integer, ForeignKey(User.id))
+
+    # a group admin can add and remove users from a group
+    admin = relationship(User, back_populates='administrated_groups')
+
+    users = relationship('User', secondary=GroupMembers,
+                         back_populates='groups')
+    events = relationship(
+        'Event', secondary=GroupEventRelations, back_populates='groups')
+
+
+class EventUpdate(Base):
+    __tablename__ = 'EventUpdate'
+    id = Column(Integer, primary_key=True)
+    message = Column(String)
+    event_id = Column(Integer, ForeignKey(Event.id))
+    created_at = Column(DateTime)
+
+    event = relationship(Event, back_populates='updates')
+
+
+Base.metadata.create_all(engine)
+db_session = scoped_session(sessionmaker(bind=engine))
