914a36600dda7793bd1c9c56c214736400d0f389
boeschm <boeschm@student.ethz.ch>
2019-08-20 22:19:29 +0200

    List participants of event

--- app/event.py
+++ app/event.py
@@ -5,7 +5,7 @@ from . import auth
 from werkzeug.exceptions import NotFound
 import pytz
 
-bp = Blueprint("event", __name__, url_prefix="/event")
+bp = Blueprint("event", __name__, url_prefix="/events")
 
 
 @bp.route('/', methods=['GET'])
@@ -16,8 +16,7 @@ def list():
 
 
 @bp.route('/create', methods=['GET', 'POST'], defaults={'id': None})
-@bp.route('/edit/<int:id>', methods=['GET', 'POST'])
-@auth.login_required
+@bp.route('/<int:id>/edit', methods=['GET', 'POST'])
 def edit(id):
     if id is None:
         event = Event()
@@ -43,8 +42,32 @@ def edit(id):
     return render_template('event/edit.html', form=form)
 
 
-@bp.route('/delete/<int:id>', methods=['GET'])
+@bp.route('/<int:event_id>/list_participants', methods=['GET'])
 @auth.login_required
+def list_participants(event_id):
+    event = db_session.query(Event).filter_by(id=event_id).first()
+    if event is None:
+        abort(NotFound)
+    else:
+        num_participants = 0
+        num_car_seats = 0
+        num_friends = 0
+
+        invitations = event.invitations
+        for inv in invitations:
+            if inv.accepted:
+                num_participants += 1
+                if inv.num_car_seats is not None:
+                    num_car_seats += inv.num_car_seats
+                if inv.num_friends is not None:
+                    num_friends += inv.num_friends
+
+        return render_template(
+            'event/list_participants.html', event=event, invitations=invitations,
+            num_participants=num_participants, num_car_seats=num_car_seats, num_friends=num_friends)
+
+
+@bp.route('/<int:id>/delete', methods=['GET'])
 def delete(id):
     event = db_session.query(Event).filter_by(id=id).first()
     if event is None:
